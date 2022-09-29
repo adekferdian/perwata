7f8605903f38adca2e4650380bb6b3b944af48e8
adekferdian <adekferdian@gmail.com>
2022-09-29 20:06:22 +0700

    command files

--- client/pages/loginOne.js
+++ client/pages/loginOne.js
@@ -1,13 +1,13 @@
-import LoginFormOne from "components/Form/LoginFormOne"
+// import LoginFormOne from "components/Form/LoginFormOne"
 
-const LoginOne = () => {
-  return (
-    <div className="auth-wrapper auth-v1 px-2">
-      <div className="auth-inner py-2">
-        <LoginFormOne />
-      </div>
-    </div>
-  )
-}
+// const LoginOne = () => {
+//   return (
+//     <div className="auth-wrapper auth-v1 px-2">
+//       <div className="auth-inner py-2">
+//         <LoginFormOne />
+//       </div>
+//     </div>
+//   )
+// }
 
-export default LoginOne
+// export default LoginOne
