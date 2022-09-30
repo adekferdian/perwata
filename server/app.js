4f2fb2310a9c1ddc062657cbfaaf0a57c07b73a6
adekferdian <adekferdian@gmail.com>
2022-09-30 18:59:07 +0700

    update server

--- server/app.js
+++ server/app.js
@@ -5,8 +5,6 @@ const PORT = process.env.PORT || 3001;
 const cors = require("cors");
 const routes = require("./routes");
 
-
-
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
